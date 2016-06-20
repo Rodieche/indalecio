2c0176e38cdc6f937a31a2d92ce95431223406e2
Rodolfo Echenique <echeniquer@gmail.com>
2016-06-20 04:31:58 -0300

    Se añaden los public al server y se empieza a modificar la pagina index del server

--- SERVER/app.js
+++ SERVER/app.js
@@ -1,8 +1,18 @@
+//	VARS
 var express = require('express');
 var app = express();
 
+//	SET THE VIEW ENGINE --> JADE
+app.set("view engine","jade");
+
+//	SET STATIC FOLDER --> ASSETS
+app.use(express.static("public"));
+
+//	BODY OF THE APLICATION
 app.get("/",function(req,res){
-	res.end("Hola mundo");
+	res.render("index");
 });
 
+
+//	PORT TO LISTEN
 app.listen(8090);
